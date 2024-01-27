837bba8856873a86b8d851aac472a11ab8d84536
webra <61615298+wiuid@users.noreply.github.com>
2024-01-27 15:38:06 +0800

    Update webra_top.py

--- webra_top.py
+++ webra_top.py
@@ -1,16 +1,15 @@
 """
-@author: webra
+@author: webra/lzm
 @time: 2023/3/27 13:31
 @description:  热榜
-@update: 2023/12/16 12:27:29
+@update: 2024/01/27 15:30:00
 """
 import glob
 import re
 import time
-
+import datetime
 
 import requests
-import datetime
 import json
 from lxml import etree
 import random
@@ -22,10 +21,10 @@ from flask import Flask
 if not os.path.exists('./data'):
     os.makedirs('./data')
 
-
-# 24小时更新一次
+# 12小时更新一次
 global_timeout_file = 24
-# 讯代理，此设置不再生效
+# 讯代理
+#proxy_address = "http://api.xdaili.cn/xdaili-api//greatRecharge/getGreatIp?spiderId=f1ed1b2a97294f74b34002bdccaedcd0&orderno=YZ2018954898PnB882&returnType=2&count=1"
 proxy_address = ""
 # web服务启动
 app = Flask(__name__)
@@ -45,35 +44,26 @@ def del_file(file_path):
 
 
 # 以utf-8编码写入文件
-def write_file(file_path, content, input_type="w"):
-    with open(file_path, input_type, encoding='utf-8') as f:
+def write_file(file_path, content, type=None):
+    type = 'w' if type is None else 'a'
+    with open(file_path, type, encoding='utf-8') as f:
         f.write(content)
 
 # 根目录提示
 @app.route("/")
 def index():
-    data_dict = {"wuai": "吾爱热榜",
-                 "zhihu": "知乎热榜",
-                 "bili/day": "bilibili全站日榜",
-                 "bili/hot": "bilibili热搜榜",
-                 "acfun": "acfun热榜",
-                 "hupu": "虎扑步行街热榜",
-                 "smzdm": "什么值得买热榜",
-                 "weibo": "微博热榜",
-                 "tieba": "百度贴吧热榜",
-                 "weixin": "微信热榜",
-                 "ssp": "少数派热榜",
-                 "36k/renqi": "36氪人气榜",
-                 "36k/zonghe": "36氪综合榜",
-                 "36k/shoucang": "36氪收藏榜",
-                 "baidu": "百度热榜",
-                 "douyin": "抖音热榜",
-                 "csdn": "CSDN热榜",
-                 "douyin": "抖音热榜",
-                 "ghbk": "果核剥壳最新数据",
-                 "history": "历史上的今天",
-                 "douban": "豆瓣新片榜"
-                 }
+    data_dict = {"get_wuai_data": "吾爱热榜",
+                 "get_zhihu_data": "知乎热榜",
+                 "get_bilibili_data": "bilibili全站日榜",
+                 "get_bilibili_hot": "bilibili热搜榜",
+                 "get_acfun_data": "acfun热榜",
+                 "get_hupu_data": "虎扑步行街热榜",
+                 "get_smzdm_data": "什么值得买热榜",
+                 "get_weibo_data": "微博热榜",
+                 "get_tieba_data": "百度贴吧热榜",
+                 "get_weixin_data": "微信热榜",
+                 "get_ssp_data": "少数派热榜",
+                 "get_36k_data": "36Kr热榜"}
     json_data = {}
     json_data["secure"] = True
     json_data["title"] = "热榜接口"
@@ -99,11 +89,10 @@ def random_user_agent():
 
 # 获取代理IP
 def get_proxy_ip():
-    url = "http://api.89ip.cn/tqdl.html?api=1&num=1&port=443&address=&isp="
+    url = "http://api.89ip.cn/tqdl.html?api=1&num=1&port=&address=&isp="
     response = get_html(url, None, "obj")
     reg = r'([0-9.:]+)<br>'
     return re.compile(reg).search(response.text).group(1)
-
 
 """
 url：字符串类型，表示要访问的网址。
@@ -112,7 +101,9 @@ res_data_type：字符串类型，指示响应数据类型，只能是html、dic
 """
 def get_html(url, headers, res_data_type, proxy_ip=None):
     # 如果headers是字符串类型，则使用该字符串作为HTTP请求头referer的值
-    if isinstance(headers, str):
+    if isinstance(headers, dict):
+        pass
+    elif isinstance(headers, str):
         headers = {
             'referer': headers,
             'User-Agent': random_user_agent()}
@@ -125,12 +116,14 @@ def get_html(url, headers, res_data_type, proxy_ip=None):
     if proxy_ip is None:
         response = requests.get(url, headers=headers, verify=False)
     else:
-        write_file("./nohup.out", datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "获取到的代理ip： " + proxy_ip, "a")
-        proxy = {
-            'https': proxy_ip
-        }
-        response = requests.get(url, headers=headers, verify=False, proxies=proxy)
-
+        try:
+            proxy = {
+                'https': proxy_ip
+            }
+            response = requests.get(url, headers=headers, verify=False, proxies=proxy)
+        except:
+            print("获取到的代理ip：" + proxy_ip)
+            return 1
     # 根据请求数据类型做数据处理
     if res_data_type == "html":
         return etree.HTML(response.text)
@@ -138,6 +131,24 @@ def get_html(url, headers, res_data_type, proxy_ip=None):
         return json.loads(response.text)
     elif res_data_type == "obj":
         return response
+
+def post_html(url, headers, data, res_data_type="dict"):
+    if isinstance(headers, str):
+        headers = {
+            'referer': headers,
+            'User-Agent': random_user_agent()}
+    # 如果headers为None，则headers中只有UA信息
+    elif headers is None:
+        headers = {'User-Agent': random_user_agent()}
+
+    # 发送POST请求
+    response = requests.post(url, headers=headers, json=data)
+
+    if res_data_type == "html":
+        return etree.HTML(response.text)
+    elif res_data_type == "dict":
+        return json.loads(response.text)
+
 
 
 # 获取文件中的数据，如果是过期数据则删除返回None，
@@ -190,7 +201,7 @@ def headle_html_data_list(datas, url_prefix="", hot_prefix=None):
         num += 1
         # 热度值均高于1W的情况，简化输出
         if hot_prefix == 10000:
-            hot = "{}{}".format(round(int(hot.strip()) / 10000, 1), "万")
+            hot = "{}{}".format(round(int(hot.strip()) / 10000, 1), "W")
         # 需要删除前后字符的情况
         elif isinstance(hot_prefix, int):
             hot = hot[:hot_prefix]
@@ -261,6 +272,7 @@ def csdn():
     else:
         return file_content
 
+
 @app.route('/wuai')
 def wuai():
     filename = "wuai_data_*.data"
@@ -287,6 +299,7 @@ def wuai():
         return end_json_data(json_data, data_list, filename)
     else:
         return file_content
+
 
 # 知乎热榜
 @app.route("/zhihu")
@@ -632,8 +645,7 @@ def history():
             title = etree.tostring(title, encoding='unicode')
             cleaned_text = re.sub(r'<.*?>|\n', '', title)
             data_dict = {"index": articles_title_num - num,
-                             "title": cleaned_text,
-                             "url": "javaScript:;"}
+                             "title": cleaned_text}
             data_list.insert(0, data_dict)
             num += 1
         return end_json_data(json_data, data_list, filename)
@@ -665,6 +677,214 @@ def douban():
     else:
         return file_content
 
+@app.route("/it/<rank>")
+def it_home(rank):
+    if rank == "day":
+        rank_num = "2"
+        rank_name = "日"
+    elif rank == "week":
+        rank_num = "4"
+        rank_name = "周"
+    elif rank == "hot":
+        rank_num = "6"
+        rank_name = "热评"
+    else:
+        rank = "month"
+        rank_num = "8"
+        rank_name = "月"
+    # 2 是日榜
+    # 4 是周榜
+    # 6 是热评榜
+    # 8 是月榜
+    filename = "it_" + rank + "_data_*.data"
+    file_content = get_file_data(filename)
+    if file_content is None:
+        json_data = init_json_data("IT之家_" + rank_name + "榜")
+        request = get_html('https://m.ithome.com/rankm/', 'https://m.ithome.com/rankm/', "html")
+        articles_title = request.xpath('/html/body/div[1]/div[2]/div[' + rank_num + ']/div/a/div[2]/p[1]/text()')
+        articles_url = request.xpath('/html/body/div[1]/div[2]/div[' + rank_num + ']/div/a/@href')
+        articles_hot = request.xpath('/html/body/div[1]/div[2]/div[' + rank_num + ']/div/a/div[2]/p[2]/span[3]/span/text()')
+        datas = [articles_title, articles_url, articles_hot]
+        return end_json_data(json_data, headle_html_data_list(datas), filename)
+    else:
+        return file_content
+
+
+@app.route("/tencent")
+def tencent():
+    filename = "tennet_new_data_*.data"
+    file_content = get_file_data(filename)
+    if file_content is None:
+        json_data = init_json_data("腾讯新闻热点榜")
+        request = get_html("https://i.news.qq.com/gw/event/pc_hot_ranking_list?ids_hash=&offset=0&page_size=50&appver=15.5_qqnews_7.1.60&rank_id=hot", None, "dict")
+        data_list = []
+        for key in request["idlist"][0]["newslist"][1:51]:
+            data_dict = {"index": key["ranking"],
+                         "title": key["title"],
+                         "url": key["url"],
+                         "hot": "{}{}".format(round(int(key["hotEvent"]["hotScore"]) / 10000, 1), "W")}
+            # hot = "{}{}".format(round(int(key["hotEvent"]["hotScore"]) / 10000, 1), "W")
+            data_list.append(data_dict)
+        return end_json_data(json_data, data_list, filename)
+    else:
+        return file_content
+
+
+
+@app.route("/wxbook/<rank>")
+def wxbook(rank):
+    if rank == "soar":
+        rank_name = "飙升"
+        rank_url = "https://weread.qq.com/web/category/rising"
+    elif rank == "new":
+        rank_name = "新书"
+        rank_url = "https://weread.qq.com/web/category/newbook"
+    elif rank == "god":
+        rank_name = "神作"
+        rank_url = "https://weread.qq.com/web/category/newrating_publish"
+    elif rank == "novel":
+        rank_name = "小说"
+        rank_url = "https://weread.qq.com/web/category/general_novel_rising"
+    elif rank == "hot":
+        rank_name = "热搜"
+        rank_url = "https://weread.qq.com/web/category/hot_search"
+    elif rank == "potential":
+        rank_name = "潜力"
+        rank_url = "https://weread.qq.com/web/category/newrating_potential_publish"
+    else:
+        rank = "all"
+        rank_name = "总"
+        rank_url = "https://weread.qq.com/web/category/all"
+
+    filename = "wx_book_" + rank + "_data_*.data"
+    file_content = get_file_data(filename)
+    if file_content is None:
+        json_data = init_json_data("微信读书_" + rank_name + "榜")
+        request = get_html(rank_url, rank_url, "html")
+        articles_title = request.xpath('//*[@id="routerView"]/div[2]/div[2]/ul/li/div[1]/div[2]/p[1]/text()')
+        articles_url = request.xpath('//*[@id="routerView"]/div[2]/div[2]/ul/li/a/@href')
+        articles_hot = request.xpath('//*[@id="routerView"]/div[2]/div[2]/ul/li/div[1]/div[2]/p[3]/span[3]/span/text()')
+        datas = [articles_title, articles_url, articles_hot]
+        return end_json_data(json_data, headle_html_data_list(datas, "https://weread.qq.com/"), filename)
+    else:
+        return file_content
+
+
+
+
+@app.route("/qidian/<rank>")
+def qidian(rank):
+    if rank == "yuepiao":
+        rank_name = "月票"
+        rank_num = "1"
+    elif rank == "changxiao":
+        rank_name = "畅销"
+        rank_num = "2"
+    elif rank == "zhisu":
+        rank_name = "阅读指数"
+        rank_num = "3"
+    elif rank == "tuijina":
+        rank_name = "推荐"
+        rank_num = "4"
+    elif rank == "shoucang":
+        rank_name = "收藏"
+        rank_num = "5"
+    elif rank == "new":
+        rank_name = "签约作者新书"
+        rank_num = "6"
+    elif rank == "new_2":
+        rank_name = "公众作家新书"
+        rank_num = "7"
+    else:
+        rank = "yuepiao_vip"
+        rank_name = "月票_vip"
+        rank_num = "8"
+
+    filename = "qidian_" + rank + "_data_*.data"
+    file_content = get_file_data(filename)
+    if file_content is None:
+        json_data = init_json_data("起点中文网_" + rank_name + "榜")
+        request = get_html("https://www.qidian.com/rank/", "https://www.qidian.com/rank/", "html")
+        articles_title_1 = request.xpath('/html/body/div[1]/div[6]/div[2]/div[2]/div/div[' + rank_num + ']/div/ul/li/div/div[1]/h2/a/text()')
+        articles_title_2 = request.xpath('/html/body/div[1]/div[6]/div[2]/div[2]/div/div[' + rank_num + ']/div/ul/li/div[2]/a/h2/text()')
+        articles_url_1 = request.xpath('/html/body/div[1]/div[6]/div[2]/div[2]/div/div[' + rank_num + ']/div/ul/li/div/div[1]/h2/a/@href')
+        articles_url_2 = request.xpath('/html/body/div[1]/div[6]/div[2]/div[2]/div/div[' + rank_num + ']/div/ul/li/div[2]/a/@href')
+        articles_hot_1 = request.xpath('/html/body/div[1]/div[6]/div[2]/div[2]/div/div[' + rank_num + ']/div/ul/li/div/div[1]/p[1]/em/text()')
+        articles_hot_2 = request.xpath('/html/body/div[1]/div[6]/div[2]/div[2]/div/div[' + rank_num + ']/div/ul/li/div[2]/i/text()')
+        articles_title = articles_title_1 + articles_title_2
+        articles_url = articles_url_1 + articles_url_2
+        articles_hot = articles_hot_1 + articles_hot_2
+        if len(articles_hot) != 10:
+            articles_hot = [""] * 10
+        datas = [articles_title, articles_url, articles_hot]
+        return end_json_data(json_data, headle_html_data_list(datas), filename)
+    else:
+        return file_content
+
+
+
+
+
+@app.route("/zongheng/<rank>")
+def zongheng(rank):
+    if rank == "yuepiao":
+        rank_name = "月票"
+        rank_url = "https://www.zongheng.com/rank?nav=monthly-ticket&rankType=1"
+        现在 = datetime.datetime.now()
+        rank_on = now.strftime("%Y") + str(now.month)
+        data = {"cateFineId": 0,"cateType": 0,"pageNum": 1,"pageSize": 20,"period": 0,"rankNo": rank_on,"rankType": 1}
+    elif rank == "24h":
+        rank_name = "24h畅销"
+        rank_url = "https://www.zongheng.com/rank?nav=one-day&rankType=3"
+        data = {"cateFineId": 0,"cateType": 0,"pageNum": 1,"pageSize": 20,"period": 0,"rankNo": "","rankType": 3}
+    elif rank == "new":
+        rank_name = "新书"
+        rank_url = "https://www.zongheng.com/rank?nav=new-book&rankType=4"
+        data = {"cateFineId": 0,"cateType": 0,"pageNum": 1,"pageSize": 20,"period": 0,"rankNo": "","rankType": 4}
+    elif rank == "tuijian":
+        rank_name = "推荐"
+        rank_url = "https://www.zongheng.com/rank?nav=recommend&rankType=6"
+        data = {"cateFineId": 0,"cateType": 0,"pageNum": 1,"pageSize": 20,"period": 0,"rankNo": "","rankType": 6}
+    elif rank == "new_dingyue":
+        rank_name = "新书订阅"
+        rank_url = "https://www.zongheng.com/rank?nav=new-book-subscribe&rankType=9"
+        data = {"cateFineId": 0,"cateType": 0,"pageNum": 1,"pageSize": 20,"period": 0,"rankNo": "","rankType": 9}
+    else:
+        rank = "dianji"
+        rank_name = "点击"
+        rank_url = "https://www.zongheng.com/rank?nav=click&rankType=5"
+        data = {"cateFineId": 0,"cateType": 0,"pageNum": 1,"pageSize": 20,"period": 0,"rankNo": "","rankType": 5}
+
+    filename = "zongheng_" + rank + "_data_*.data"
+    file_content = get_file_data(filename)
+    if file_content is None:
+        json_data = init_json_data("纵横中文网_" + rank_name + "榜")
+        request = post_html("https://www.zongheng.com/api/rank/details", rank_url, data)
+        data_list = []
+        for key in request["result"]["resultList"]:
+            data_dict = {"index": key["orderNo"],
+                         "title": key["bookName"],
+                         "url": "https://www.zongheng.com/detail/" + str(key["bookId"]),
+                         "hot": ""}
+            data_list.append(data_dict)
+        return end_json_data(json_data, data_list, filename)
+    else:
+        return file_content
+    
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run()
+
+
+
